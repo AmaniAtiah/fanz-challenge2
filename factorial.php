284f13d3532ab5b8283715b78259eee1329ef1f5
Amaniatiah <amaniatiah@gmail.com>
2024-06-30 03:52:38 +0300

    done challenge 2

---
+++ factorial.php
@@ -0,0 +1,19 @@
+
+<?php 
+
+
+/*
+ Definition of a function called Factorial that is of integer type and has an integer parameter that returns an integer
+Setting a condition to make the function call itself back
+As long as the value of the number parameter is greater than zero, the function calls itself again and subtracts 1 from the value of the number parameter.
+ */
+
+ function factorial($number) {
+    if ($number > 0) {
+        return $number * factorial($number - 1);
+    }
+    return 1;
+}
+
+
+?>
